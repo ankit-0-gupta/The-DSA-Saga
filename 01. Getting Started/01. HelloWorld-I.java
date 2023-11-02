c90c635552c73206d50eb14c2fd64cb61617c996
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2023-11-02 20:57:52 +0530

    Hello World - I

---
+++ 01. Getting Started/01. HelloWorld-I.java
@@ -0,0 +1,10 @@
+https://practice.geeksforgeeks.org/problems/java-hello-world4004/1
+
+
+class Solution
+{
+    static void printHelloWorld()
+    {
+        System.out.print("Hello World");
+    }
+}
