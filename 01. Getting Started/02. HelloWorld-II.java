bf1d3f0371720ac80e864a9b023a268aa779c4bb
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2023-11-02 21:00:03 +0530

    Hello World - II

---
+++ 01. Getting Started/02. HelloWorld-II.java
@@ -0,0 +1,13 @@
+// https://practice.geeksforgeeks.org/problems/the-new-line-java/1
+
+
+class Geeks
+{
+    // Function to print each word in single line
+    static void printIndividualLine()
+    {
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.print("Geeks");
+    }   
+}
