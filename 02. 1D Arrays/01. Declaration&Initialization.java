b6aeaaa12cf4cc55d58b0db98123df485ba088b7
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2023-11-24 20:02:27 +0530

    Declaration And Initialization

---
+++ 02. 1D Arrays/01. Declaration&Initialization.java
@@ -0,0 +1,18 @@
+// Declaration And Initialization.
+  
+
+import java.util.*;
+class Solution 
+{
+    public static void main(String[] args)
+    {
+        // Declaration.
+        int[] arr = null;
+        System.out.println(arr);
+
+        // Declaration And Initialization.
+        int[] nums = new int[5];
+        System.out.println(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
